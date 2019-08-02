a6ac76994b3f6c7519204f910fc787b7928cf8ef
developer <dev@devops.com>
2019-08-02 17:17:29 +0200

    Changed color for menu buttons

--- code/develop_nav.sh
+++ code/develop_nav.sh
@@ -2,4 +2,7 @@
 . lib/commonLibrary.sh
 
 set_variables #Call library function
-printf "\n$GREEN\tUP\n\nLEFT\t\tRIGHT\n\n\tDOWN$RESET\n" #develop the navigation menu buttons
+
+echo $CYAN
+printf "\n\tUP\n\nLEFT\t\t\n\n\tDOWN\n" #develop the navigation menu buttons
+echo $RESET
