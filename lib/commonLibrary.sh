f61a7e8dbaa2492a03c877ffcba43eae704ded31
developer <dev@devops.com>
2019-08-07 10:39:44 +0200

    Adding clean function

--- lib/commonLibrary.sh
+++ lib/commonLibrary.sh
@@ -23,3 +23,21 @@ exit_status(){
 }
 
 
+#------------------- clean files -------------------#
+clean(){
+	filename=$1
+	if [ -f $filename ];then
+		printf "\n----------\nAre you sure you want to delete the file: '$filename' | yes or no?  "
+		read response
+		if [ ! -z ` echo $response | egrep -iw "y|Y|Yes|YES|yES|yes" ` ];then
+			rm -f $filename
+			printf "User adviced, '$filename' file deleted"
+		else
+			printf "User denied, '$filename' file not deleted"
+		fi
+	else
+		printf "\n----------\n'$filename' filename not found, unable to delete\n"
+	fi
+	printf "\n"
+	exit_status 0
+}
