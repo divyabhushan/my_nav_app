1544bc6203079b2c475fcac2fee25837479ef849
developer <dev@devops.com>
2019-08-07 15:16:40 +0200

    Revert "Changed color for menu buttons"
    This reverts commit a6ac76994b3f6c7519204f910fc787b7928cf8ef.

--- code/develop_nav.sh
+++ code/develop_nav.sh
@@ -2,7 +2,4 @@
 . lib/commonLibrary.sh
 
 set_variables #Call library function
-
-echo $CYAN
-printf "\n\tUP\n\nLEFT\t\t\n\n\tDOWN\n" #develop the navigation menu buttons
-echo $RESET
+printf "\n$GREEN\tUP\n\nLEFT\t\tRIGHT\n\n\tDOWN$RESET\n" #develop the navigation menu buttons
